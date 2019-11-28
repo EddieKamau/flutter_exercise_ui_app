558799dea6b8de8fd19c06ca5bdcbe9485af2b91
EddieKamau <elyudde@gmail.com>
2019-11-28 10:17:45 +0300

    added BottomNavBar

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # exercise_ui
 
-A new Flutter project.
+### Screenshot 1
+![Screenshot](extras/Screenshot_20191128-095938.png)
 
-## Getting Started
+### Screenshot 2
+![Screenshot](extras/Screenshot_20191128-095952.png)
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### GIF
+<img src="extras/vid.gif" width=450>
--- lib/main.dart
+++ lib/main.dart
@@ -9,156 +9,197 @@ void main() {
   home: MainApp()
   ));
 }
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _currentBottomNavItem = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Color.fromRGBO(44, 44, 84,1.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // Humbergger icon
-          IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.menu, color: Colors.white70,),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (value){
+          setState(() {
+            _currentBottomNavItem = value;
+          });
+        },
+        currentIndex: _currentBottomNavItem,
+        elevation: 0,
+        backgroundColor: Color.fromRGBO(44, 44, 84,1.0),
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.white70,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forward_30),
+            title: _currentBottomNavItem == 0 ? Container(height: 3, width: 14, color: Colors.blueAccent, margin: EdgeInsets.only(top: 2),) : Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: _currentBottomNavItem == 1 ? Container(height: 3, width: 14, color: Colors.blueAccent, margin: EdgeInsets.only(top: 2),) : Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_drink),
+            title: _currentBottomNavItem == 2 ? Container(height: 3, width: 14, color: Colors.blueAccent, margin: EdgeInsets.only(top: 2),) : Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flare),
+            title: _currentBottomNavItem == 3 ? Container(height: 3, width: 14, color: Colors.blueAccent, margin: EdgeInsets.only(top: 2),) : Container(),
           ),
 
-          // pageview carousel
-          Container(
-            height: 200,
-            child: PagevieCarousel(),
-          ),
+        ],
+      ),
+      body: Material(
+        color: Color.fromRGBO(44, 44, 84,1.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Humbergger icon
+            IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.menu, color: Colors.white70,),
+            ),
 
-          // Body
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(
-                children: <Widget>[
-                  // Title
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      'Daily Exercise',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 29
+            // pageview carousel
+            Container(
+              height: 200,
+              child: PagevieCarousel(),
+            ),
+
+            // Body
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(
+                  children: <Widget>[
+                    // Title
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Daily Exercise',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 29
+                        ),
                       ),
                     ),
-                  ),
 
-                  // Grid cards
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                      itemCount: 4,
-                      itemBuilder: (BuildContext context, int index){
-                        return Card(
-                          color: Colors.transparent,
-                          child: Padding(
-                            padding: EdgeInsets.all(18),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                // Top
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: <Widget>[
-                                      Icon(Icons.airline_seat_recline_extra, size: 35, color: Colors.white70,),
-                                      Text(
-                                        'Push Up',
-                                        style: TextStyle(
-                                          color: Colors.white70,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-
-                                // Bottom
-                                Expanded(
-                                  child: Align(
-                                    alignment: Alignment.bottomCenter,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
+                    // Grid cards
+                    Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                        itemCount: 4,
+                        itemBuilder: (BuildContext context, int index){
+                          return Card(
+                            color: Colors.transparent,
+                            child: Padding(
+                              padding: EdgeInsets.all(18),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  // Top
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       mainAxisSize: MainAxisSize.max,
                                       children: <Widget>[
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: <Widget>[
-                                            RichText(
-                                              maxLines: 1,
-                                              text: TextSpan(
-                                                children: [
-                                                  TextSpan(
-                                                    text: '33',
-                                                    style: TextStyle(
-                                                      color: Colors.white38,
-                                                      fontWeight: FontWeight.bold,
-                                                      fontSize: 17
-                                                    ),
-                                                  ),
-                                                  TextSpan(
-                                                    text: '/45',
-                                                    style: TextStyle(
-                                                      color: Colors.white38
-                                                    ),
-                                                  )
-                                                ]
-                                              ),
-                                            ),
-                                            Text(
-                                              'active',
-                                              style: TextStyle(
-                                                color: Colors.white38,
-                                                fontSize: 11,
-                                              ),
-                                            ),
-                                          ],
+                                        Icon(Icons.airline_seat_recline_extra, size: 35, color: Colors.white70,),
+                                        Text(
+                                          'Push Up',
+                                          style: TextStyle(
+                                            color: Colors.white70,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold
+                                          ),
                                         ),
-                                        Container(
-                                          width: 35,
-                                          height: 35,
-                                          decoration: BoxDecoration(
-                                            color: Colors.white12,
-                                            shape: BoxShape.circle
-                                          ),
-                                          child: Center(
-                                            child: Text(
-                                              '75%',
-                                              style: TextStyle(
-                                                color: Colors.white70,
-                                                fontWeight: FontWeight.bold
-                                              ),
-                                            ),
-                                          ),
-                                        )
                                       ],
                                     ),
                                   ),
-                                ),
-                              ],
+
+                                  // Bottom
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: <Widget>[
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              RichText(
+                                                maxLines: 1,
+                                                text: TextSpan(
+                                                  children: [
+                                                    TextSpan(
+                                                      text: '33',
+                                                      style: TextStyle(
+                                                        color: Colors.white38,
+                                                        fontWeight: FontWeight.bold,
+                                                        fontSize: 17
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text: '/45',
+                                                      style: TextStyle(
+                                                        color: Colors.white38
+                                                      ),
+                                                    )
+                                                  ]
+                                                ),
+                                              ),
+                                              Text(
+                                                'active',
+                                                style: TextStyle(
+                                                  color: Colors.white38,
+                                                  fontSize: 11,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Container(
+                                            width: 35,
+                                            height: 35,
+                                            decoration: BoxDecoration(
+                                              color: Colors.white12,
+                                              shape: BoxShape.circle
+                                            ),
+                                            child: Center(
+                                              child: Text(
+                                                '75%',
+                                                style: TextStyle(
+                                                  color: Colors.white70,
+                                                  fontWeight: FontWeight.bold
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
